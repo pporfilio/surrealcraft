660b866351436ff6bd34a796f1571c6c3ae0dd79
Parker Porfilio <pporfilio@gmail.com>
2022-04-17 21:20:25 -0400

    Woo blank window and I don't remember C++

--- qt_client/src/app/glwindow.cpp
+++ qt_client/src/app/glwindow.cpp
@@ -1,30 +1,119 @@
 #include "glwindow.h"
 
+#include <QOpenGLFunctions>
+#include <QOpenGLExtraFunctions>
+
 GLWindow::GLWindow()
 {
-    m_uniformMatrices.insert({m_WORLD_MATRIX_NAME, QMatrix4x4()});
+    m_uniformMatrices.insert(m_WORLD_MATRIX_NAME, QMatrix4x4());
+    m_uniformMatrices.insert(m_CAMERA_MATRIX_NAME, QMatrix4x4());
+    m_uniformMatrices.insert(m_WORLD_MATRIX_NAME, QMatrix4x4());
+    m_uniformMatrices.insert(m_MY_MATRIX_NAME, QMatrix4x4());
+    m_uniformVectors.insert(m_LIGHT_POSITION_NAME, QVector3D());
+    m_uniformVectors.insert(m_EYE_NAME, QVector3D());
+    m_uniformVectors.insert(m_TARGET_NAME, QVector3D());
+
+    m_triangle_data.append(-1.0);
+    m_triangle_data.append(1.0);
+    m_triangle_data.append(0.0);
+    m_triangle_data.append(1.0);
+    m_triangle_data.append(1.0);
+    m_triangle_data.append(0.0);
+    m_triangle_data.append(-1.0);
+    m_triangle_data.append(0.0);
+    m_triangle_data.append(0.0);
 }
 
 GLWindow::~GLWindow() {
     makeCurrent();
 }
 
+static const char *vertexShaderSource =
+    "#version 330\n"
+    "layout(location = 0) in vec4 vertex;\n"
+    "out vec3 vert;\n"
+    "out vec3 color;\n"
+    "uniform mat4 projMatrix;\n"
+    "uniform mat4 camMatrix;\n"
+    "uniform mat4 worldMatrix;\n"
+    "uniform mat4 myMatrix;\n"
+    "uniform sampler2D sampler;\n"
+    "void main() {\n"
+    "   mat4 wm = myMatrix * worldMatrix;\n"
+    "   color = vec3(1.0, 0.0, 0.0);\n"
+    "   vert = vec3(wm * vertex);\n"
+    "   gl_Position = projMatrix * camMatrix * wm * vertex;\n"
+    "}\n";
+
+static const char *fragmentShaderSource =
+    "#version 330\n"
+    "in highp vec3 vert;\n"
+    "in highp vec3 color;\n"
+    "out highp vec4 fragColor;\n"
+    "uniform highp vec3 lightPos;\n"
+    "void main() {\n"
+    "   fragColor = vec4(col, 1.0);\n"
+    "}\n";
+
 void GLWindow::initializeGL() {
     QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
 
-    // TODO load texture(s)
+    m_program.reset(new QOpenGLShaderProgram);
+    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
+    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
+    m_program->link();
 
-    // TODO load program(s)
 
-    m_uniformLocations.insert({m_PROJECTION_MATRIX_NAME, m_program->uniformLocation(m_PROJECTION_MATRIX_NAME.c_str())});
+    m_uniformLocations.insert(m_PROJECTION_MATRIX_NAME, m_program->uniformLocation(m_PROJECTION_MATRIX_NAME));
+    m_uniformLocations.insert(m_CAMERA_MATRIX_NAME, m_program->uniformLocation(m_CAMERA_MATRIX_NAME));
+    m_uniformLocations.insert(m_WORLD_MATRIX_NAME, m_program->uniformLocation(m_WORLD_MATRIX_NAME));
+    m_uniformLocations.insert(m_MY_MATRIX_NAME, m_program->uniformLocation(m_MY_MATRIX_NAME));
+    m_uniformLocations.insert(m_LIGHT_POSITION_NAME, m_program->uniformLocation(m_LIGHT_POSITION_NAME));
+
+    m_vertexArrayObject.reset(new QOpenGLVertexArrayObject);
+    if (m_vertexArrayObject->create()) {
+        m_vertexArrayObject->create();
+    }
+
+    m_program->bind();
+    m_vertexBuffer.reset(new QOpenGLBuffer);
+    m_vertexBuffer->create();
+    m_vertexBuffer->bind();
+
+    // This copies the contents of the first parameter to the GPU
+    m_vertexBuffer->allocate(m_triangle_data.constData(), m_triangle_data.size() * sizeof(GLfloat));
+
+    // This enables `layout(location = 0) in vec4 vertex;` from the shader
+    f->glEnableVertexAttribArray(0);
+
+    // This says that layout(location = 0) starts at 0 (nullptr) offset into the VBO
+    f->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), nullptr);
+
+    m_vertexBuffer->release();
 
 
 }
 
 void GLWindow::resizeGL(int w, int h) {
-
+    m_uniformMatrices.insert(m_PROJECTION_MATRIX_NAME, QMatrix4x4());
+    QMatrix4x4 tmp;
+    tmp.perspective(45.0f, GLfloat(w) / h, 0.01f, 100.0f);
+    m_uniformMatrices.insert(m_PROJECTION_MATRIX_NAME, tmp);
 }
 
 void GLWindow::paintGL() {
+    QOpenGLExtraFunctions *f = QOpenGLContext::currentContext()->extraFunctions();
+    f->glClearColor(0, 0, 0, 1);
+    f->glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    m_program->bind();
+    QMatrix4x4 tmp;
+    tmp.lookAt(QVector3D(0, 0, -1), QVector3D(0, 0, 0), QVector3D(0, 1, 0));
+    m_uniformMatrices.insert(m_CAMERA_MATRIX_NAME, tmp);
+    m_program->setUniformValue(m_uniformLocations.value(m_PROJECTION_MATRIX_NAME), m_uniformMatrices.value(m_PROJECTION_MATRIX_NAME));
+    m_program->setUniformValue(m_uniformLocations.value(m_CAMERA_MATRIX_NAME), m_uniformMatrices.value(m_CAMERA_MATRIX_NAME));
+    m_program->setUniformValue(m_uniformLocations.value(m_WORLD_MATRIX_NAME), m_uniformMatrices.value(m_WORLD_MATRIX_NAME));
+    m_program->setUniformValue(m_uniformLocations.value(m_MY_MATRIX_NAME), m_uniformMatrices.value(m_MY_MATRIX_NAME));
+
+    f->glDrawArrays(GL_TRIANGLES, 0, m_triangle_data.size() / 3);
 }
--- qt_client/src/app/glwindow.h
+++ qt_client/src/app/glwindow.h
@@ -32,22 +32,24 @@ public:
     void paintGL();
 
 private:
-    const std::string m_PROJECTION_MATRIX_NAME = std::string("projectionMatrix");
-    const std::string m_CAMERA_MATRIX_NAME = std::string("cameraMatrix");
-    const std::string m_WORLD_MATRIX_NAME = std::string("worldMatrix");
-    const std::string m_MY_MATRIX_NAME = std::string("myMatrix");
-    const std::string m_LIGHT_POSITION_NAME = std::string("lightPosition");
-    const std::string m_EYE_NAME = std::string("eye");
-    const std::string m_TARGET_NAME = std::string("target");
+    const QString m_PROJECTION_MATRIX_NAME = QString("projectionMatrix");
+    const QString m_CAMERA_MATRIX_NAME = QString("cameraMatrix");
+    const QString m_WORLD_MATRIX_NAME = QString("worldMatrix");
+    const QString m_MY_MATRIX_NAME = QString("myMatrix");
+    const QString m_LIGHT_POSITION_NAME = QString("lightPosition");
+    const QString m_EYE_NAME = QString("eye");
+    const QString m_TARGET_NAME = QString("target");
 
     std::unique_ptr<QOpenGLTexture> m_texture;
     std::unique_ptr<QOpenGLShaderProgram> m_program;
     std::unique_ptr<QOpenGLBuffer> m_vertexBuffer;
     std::unique_ptr<QOpenGLVertexArrayObject> m_vertexArrayObject;
 
-    std::unordered_map<std::string, int> m_uniformLocations;
-    std::unordered_map<std::string, QMatrix4x4> m_uniformMatrices;
-    std::unordered_map<std::string, QVector3D> m_uniformVectors;
+    QMap<QString, int> m_uniformLocations;
+    QMap<QString, QMatrix4x4> m_uniformMatrices;
+    QMap<QString, QVector3D> m_uniformVectors;
+
+    QList<GLfloat> m_triangle_data;
 
 };
 
