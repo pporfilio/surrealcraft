366f5d5e7e21379c460b1a7aedcfd92c6b09cbb6
Parker Porfilio <pporfilio@gmail.com>
2022-10-02 22:55:54 -0400

    Handled window resizing.

--- rust_client/src/main_window/main_window.rs
+++ rust_client/src/main_window/main_window.rs
@@ -17,6 +17,14 @@ pub async fn run() {
             ref event,
             window_id,
         } if window_id == window.id() => match event {
+            WindowEvent::Resized(physical_size) => {
+                state.resize(*physical_size);
+            }
+            // I have no idea what this syntax means...
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                 // new_inner_size is &&mut so we have to dereference it twice
+                state.resize(**new_inner_size);
+            }
             WindowEvent::CloseRequested
             | WindowEvent::KeyboardInput {
                 input:
