896446d8a7c33f08ad245bf07f8efeb9b462c414
Parker Porfilio <pporfilio@gmail.com>
2022-10-17 22:03:04 -0400

    Added depth buffer.

--- rust_client/src/main_window/wgpu_state.rs
+++ rust_client/src/main_window/wgpu_state.rs
@@ -1,6 +1,7 @@
 use super::buffers::GeometryBuffers;
 use super::buffers::RawMatrix;
 use super::buffers::Vertex;
+use super::texture;
 use cgmath::SquareMatrix;
 use wgpu::util::DeviceExt;
 use winit::window::Window;
@@ -71,6 +72,7 @@ pub struct WGPUState {
     pub size: winit::dpi::PhysicalSize<u32>,
     pub render_pipeline: wgpu::RenderPipeline,
     pub camera_uniform: Matrix4UniformInfo,
+    pub depth_texture: texture::Texture,
 }
 
 // https://sotrh.github.io/learn-wgpu/beginner/tutorial2-surface/#first-some-housekeeping-state
@@ -125,6 +127,9 @@ impl WGPUState {
             present_mode: wgpu::PresentMode::Fifo,
         };
         surface.configure(&device, &config);
+
+        let depth_texture =
+            texture::Texture::create_depth_texture(&device, &config, "depth_texture");
 
         // Can be shortened to
         // let shader = device.create_shader_module(include_wgsl!("shaders/shader.wgsl"));
@@ -183,7 +188,13 @@ impl WGPUState {
                 // Requires Features::CONSERVATIVE_RASTERIZATION
                 conservative: false,
             },
-            depth_stencil: None,
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: texture::Texture::DEPTH_FORMAT,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
             multisample: wgpu::MultisampleState {
                 count: 1,
                 mask: !0,
@@ -200,6 +211,7 @@ impl WGPUState {
             size,
             render_pipeline,
             camera_uniform,
+            depth_texture,
         }
     }
 
@@ -209,6 +221,8 @@ impl WGPUState {
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
+            self.depth_texture =
+                texture::Texture::create_depth_texture(&self.device, &self.config, "depth_texture");
         }
     }
 
@@ -259,7 +273,14 @@ impl WGPUState {
                         },
                     }),
                 ],
-                depth_stencil_attachment: None,
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: true,
+                    }),
+                    stencil_ops: None,
+                }),
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
