926828fa72d93fe2aa5101e87d0b68ede9fe3c1e
Parker Porfilio <530116+pporfilio@users.noreply.github.com>
2025-08-19 17:33:59 -0400

    Factoring things out of the experiments main funtion.

--- experiments/src/main.rs
+++ experiments/src/main.rs
@@ -6,6 +6,32 @@ use cgmath::Vector2;
 use image;
 mod base_noise;
 
+fn test_1d_noise(imgx: u32, imgy: u32, random_state: &RandomState, output_path: &str) {
+    // Visualize a sequence of floats as a 2d plot
+    // noise_1d produces a smooth transition of values between integers
+    let mut imgbuf = image::RgbImage::new(imgx, imgy);
+    for x in 0..imgx - 1 {
+        let y = base_noise::noise_1d(&random_state, x as f32 / 40.0, false);
+        // println!("{}", y);
+        let pixel = imgbuf.get_pixel_mut(x, (y * imgy as f32).floor() as u32);
+        *pixel = image::Rgb([255, 255, 255]);
+    }
+    imgbuf.save(output_path).unwrap();
+}
+
+fn test_2d_value_noise(imgx: u32, imgy: u32, random_state: &RandomState, output_path: &str) {
+    // Generate an image with random values at integer locations and smooth
+    // transitions between. Looks like a blurred grid of squares
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let value = base_noise::noise_2d(&random_state, Vector2::new(x as f32 / 40.0, y as f32 / 40.0), false);
+        *pixel = image::Luma([(value * 80.0).floor() as u8]);
+    }
+    imgbuf.save(output_path).unwrap();
+}
+
+
+
 fn main() {
     // Creating a hasher from RandomState with a particular seed always
     // hashes the same inputs to the same outputs.
@@ -16,25 +42,9 @@ fn main() {
     let imgx = 800;
     let imgy = 800;
 
-    // Visualize a sequence of floats as a 2d plot
-    // noise_1d produces a smooth transition of values between integers
-    let mut imgbuf = image::RgbImage::new(imgx, imgy);
-    for x in 0..imgx - 1 {
-        let y = base_noise::noise_1d(&s, x as f32 / 40.0, false);
-        // println!("{}", y);
-        let pixel = imgbuf.get_pixel_mut(x, (y * imgy as f32).floor() as u32);
-        *pixel = image::Rgb([255, 255, 255]);
-    }
-    imgbuf.save("smooth_value_noise_1d.png").unwrap();
+    test_1d_noise(imgx, imgy, &s, "smooth_value_noise_1d.png");
 
-    // Generate an image with random values at integer locations and smooth
-    // transitions between. Looks like a blurred grid of squares
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let value = base_noise::noise_2d(&s, Vector2::new(x as f32 / 40.0, y as f32 / 40.0), false);
-        *pixel = image::Luma([(value * 80.0).floor() as u8]);
-    }
-    imgbuf.save("smooth_value_noise_2d.png").unwrap();
+    test_2d_value_noise(imgx, imgy, &s, "smooth_value_noise_2d.png");
 
     // Generate an image of a single layer of perlin noise
     let mut imgbuf = image::RgbImage::new(imgx, imgy);
@@ -42,7 +52,8 @@ fn main() {
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         let value = base_noise::perlin_layer_2d(
             &s,
-            Vector2::new(x as f32 / 400.0, y as f32 / 400.0),
+            // Vector2::new(x as f32 / 400.0, y as f32 / 400.0),
+            Vector2::new(x as f32 / 200.0, y as f32 / 200.0),
             false,
         );
         if value < -0.01 {
