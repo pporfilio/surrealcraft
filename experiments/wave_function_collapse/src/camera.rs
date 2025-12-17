fb9d4e48d68ee20b6f8b28dbe6bec6ef4199e396
Parker Porfilio <530116+pporfilio@users.noreply.github.com>
2025-12-16 23:13:34 -0500

    Added orthographic camera with 2d movement.

--- experiments/wave_function_collapse/src/camera.rs
+++ experiments/wave_function_collapse/src/camera.rs
@@ -97,8 +97,8 @@ impl CameraUniform {
         });
     }
 
-    pub fn update_view_proj(&mut self, camera: &Camera) {
-        self.view_proj = camera.build_view_projection_matrix().into();
+    pub fn set_view_proj(&mut self, view_proj_matrix: cgmath::Matrix4<f32>) {
+        self.view_proj = view_proj_matrix.into();
     }
 }
 
@@ -172,6 +172,107 @@ impl CameraController {
         }
         if self.is_left_pressed {
             camera.eye = camera.target - (forward - right * self.speed).normalize() * forward_mag;
+        }
+    }
+}
+
+pub struct OrthoCamera2D {
+    pub eye: cgmath::Point3<f32>,
+    pub target: cgmath::Point3<f32>,
+    pub up: cgmath::Vector3<f32>,
+    pub left: f32,
+    pub right: f32,
+    pub top: f32,
+    pub bottom: f32,
+    pub znear: f32,
+    pub zfar: f32,
+    pub speed: f32,
+    pub is_up_pressed: bool,
+    pub is_down_pressed: bool,
+    pub is_left_pressed: bool,
+    pub is_right_pressed: bool,
+    pub is_zoom_in_pressed: bool,
+    pub is_zoom_out_pressed: bool,
+}
+
+impl OrthoCamera2D {
+    pub fn new(left: f32, right: f32, top: f32, bottom: f32, speed: f32) -> Self {
+        Self {
+            // position the camera 1 unit up and 2 units back
+            // +z is out of the screen
+            eye: (0.0, 0.0, 2.0).into(),
+            // have it look at the origin
+            target: (0.0, 0.0, 0.0).into(),
+            // which way is "up"
+            up: cgmath::Vector3::unit_y(),
+            left,
+            right,
+            top,
+            bottom,
+            znear: 0.1,
+            zfar: 100.0,
+            speed,
+            is_up_pressed: false,
+            is_down_pressed: false,
+            is_left_pressed: false,
+            is_right_pressed: false,
+            is_zoom_in_pressed: false,
+            is_zoom_out_pressed: false,
+        }
+    }
+
+    pub fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
+        let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
+        let proj = cgmath::ortho(
+            self.left,
+            self.right,
+            self.bottom,
+            self.top,
+            self.znear,
+            self.zfar,
+        );
+
+        return OPENGL_TO_WGPU_MATRIX * proj * view;
+    }
+
+    pub fn handle_key(&mut self, key: KeyCode, is_pressed: bool) -> bool {
+        match key {
+            KeyCode::KeyW | KeyCode::ArrowUp => {
+                self.is_up_pressed = is_pressed;
+                true
+            }
+            KeyCode::KeyA | KeyCode::ArrowLeft => {
+                self.is_left_pressed = is_pressed;
+                true
+            }
+            KeyCode::KeyS | KeyCode::ArrowDown => {
+                self.is_down_pressed = is_pressed;
+                true
+            }
+            KeyCode::KeyD | KeyCode::ArrowRight => {
+                self.is_right_pressed = is_pressed;
+                true
+            }
+            _ => false,
+        }
+    }
+
+    pub fn update_camera(&mut self) {
+        if self.is_up_pressed {
+            self.eye += cgmath::Vector3::new(0.0, self.speed, 0.0);
+            self.target += cgmath::Vector3::new(0.0, self.speed, 0.0);
+        }
+        if self.is_down_pressed {
+            self.eye -= cgmath::Vector3::new(0.0, self.speed, 0.0);
+            self.target -= cgmath::Vector3::new(0.0, self.speed, 0.0);
+        }
+        if self.is_right_pressed {
+            self.eye += cgmath::Vector3::new(self.speed, 0.0, 0.0);
+            self.target += cgmath::Vector3::new(self.speed, 0.0, 0.0);
+        }
+        if self.is_left_pressed {
+            self.eye -= cgmath::Vector3::new(self.speed, 0.0, 0.0);
+            self.target -= cgmath::Vector3::new(self.speed, 0.0, 0.0);
         }
     }
 }
--- experiments/wave_function_collapse/src/main.rs
+++ experiments/wave_function_collapse/src/main.rs
@@ -33,11 +33,10 @@ pub struct State {
     demo_state: alg::DemoState,
     diffuse_bind_group: wgpu::BindGroup,
     diffuse_texture: texture::Texture,
-    camera: camera::Camera,
+    camera: camera::OrthoCamera2D,
     camera_uniform: camera::CameraUniform,
     camera_buffer: wgpu::Buffer,
     camera_bind_group: wgpu::BindGroup,
-    camera_controller: camera::CameraController,
 }
 
 // Rendering based on https://sotrh.github.io/learn-wgpu/beginner/
@@ -116,24 +115,10 @@ impl State {
         });
         let num_indices = INDICES.len() as u32;
 
-        let camera = camera::Camera {
-            // position the camera 1 unit up and 2 units back
-            // +z is out of the screen
-            eye: (0.0, 1.0, 2.0).into(),
-            // have it look at the origin
-            target: (0.0, 0.0, 0.0).into(),
-            // which way is "up"
-            up: cgmath::Vector3::unit_y(),
-            aspect: surface_config.width as f32 / surface_config.height as f32,
-            fovy: 45.0,
-            znear: 0.1,
-            zfar: 100.0,
-        };
-
-        let camera_controller = camera::CameraController::new(0.2);
+        let camera = camera::OrthoCamera2D::new(-10.0, 10.0, 10.0, -10.0, 1.0);
 
         let mut camera_uniform = camera::CameraUniform::new();
-        camera_uniform.update_view_proj(&camera);
+        camera_uniform.set_view_proj(camera.build_view_projection_matrix());
 
         let camera_buffer = camera_uniform.get_camera_buffer(&device);
 
@@ -171,7 +156,6 @@ impl State {
             camera_uniform,
             camera_buffer,
             camera_bind_group,
-            camera_controller,
         })
     }
 
@@ -325,7 +309,7 @@ impl State {
             (KeyCode::Escape, true) => event_loop.exit(),
             (KeyCode::Space, true) => self.step_algorithm(),
             _ => {
-                self.camera_controller.handle_key(code, is_pressed);
+                self.camera.handle_key(code, is_pressed);
             }
         }
     }
@@ -357,8 +341,9 @@ impl State {
     }
 
     pub fn update(&mut self) {
-        self.camera_controller.update_camera(&mut self.camera);
-        self.camera_uniform.update_view_proj(&self.camera);
+        self.camera.update_camera();
+        self.camera_uniform
+            .set_view_proj(self.camera.build_view_projection_matrix());
         self.queue.write_buffer(
             &self.camera_buffer,
             0,
